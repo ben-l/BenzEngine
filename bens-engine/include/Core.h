3954463d961bd50fff572bcba627c8ec83d31062
Ben Lewin <benlewin7@gmail.com>
2020-12-29 17:34:50 +0000

    added Events

--- bens-engine/include/Core.h
+++ bens-engine/include/Core.h
@@ -8,6 +8,11 @@
 #error Bens Engine only supports Linux!
 #endif
 
+// binary - shift 1 by x places to the left
+// so 1 added by (x) 0s e.g. BIT(4) == 1000
+// converted to decimal is 16
+#define BIT(x) (1 << x)
+
 /*
 #ifdef BE_PLATFORM_LINUX
     #ifdef BE_BUILD_SO
