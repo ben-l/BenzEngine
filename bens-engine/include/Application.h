f36db93e0cee24cc8aeea55f1559cc0736577966
Ben Lewin <benlewin7@gmail.com>
2021-01-21 23:55:59 +0000

    defined Window Events + kill window working

--- bens-engine/include/Application.h
+++ bens-engine/include/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <bepch.h>
 #include <Core.h>
+#include <ApplicationEvent.h>
 #include <Window.h>
 
 namespace BensEngine {
@@ -11,7 +12,11 @@ namespace BensEngine {
             virtual ~Application();
 
             void Run();
+
+            void OnEvent(Event& e);
         private:
+            bool OnWindowClose(WindowCloseEvent& e);
+
             std::unique_ptr<Window> m_Window;
             bool m_Running = true;
     };
--- bens-engine/src/Application.cpp
+++ bens-engine/src/Application.cpp
@@ -1,19 +1,27 @@
 #include <bepch.h>
-#include "Application.h"
+#include <Application.h>
 #include "GLFW/glfw3.h"
-#include "Log.h"
-#include "events/ApplicationEvent.h"
+#include <Log.h>
 
 namespace BensEngine {
+    #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
     Application::Application()
     {
         m_Window = std::unique_ptr<Window>(Window::Create());
+        m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
     }
     
     Application::~Application()
     {
     }
+
+    void Application::OnEvent(Event& e){
+        EventDispatcher dispatcher(e);
+        dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+        BE_CORE_TRACE("{0}", e);
+    }
+
 
     void Application::Run()
     {
@@ -27,6 +35,10 @@ namespace BensEngine {
             glClear(GL_COLOR_BUFFER_BIT);
             m_Window->OnUpdate();
         }
+    }
+    bool Application::OnWindowClose(WindowCloseEvent& e){
+        m_Running = false;
+        return true;
     }
 
 }
